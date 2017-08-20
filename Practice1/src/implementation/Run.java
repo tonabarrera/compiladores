12b323ae477abf5b11710ebc6906930259488866
Carlos Barrera <carlostonatihu@gmail.com>
2017-08-20 16:33:00 -0500

    Practica 2

--- Practice1/src/implementation/Run.java
+++ Practice1/src/implementation/Run.java
@@ -39,31 +39,34 @@ public class Run {
         int initialState;
         String statesString;
 
-        alphabet.add('a');
-        alphabet.add('b');
-        //alphabet.add('x');
+        alphabet.add('0');
+        alphabet.add('1');
+        alphabet.add('5');
 
         states.add(0);
         states.add(1);
         states.add(2);
         states.add(3);
         states.add(4);
+        states.add(5);
+
 
 
 
         automaton.setAlphabet(alphabet);
         automaton.setStates(states);
         automaton.setInitialState(0);
-        automaton.addFinalState(2);
+        automaton.addFinalState(6);
         automaton.addFinalState(4);
 
 
-        automaton.addTransition(0, 1, 'e');
-        automaton.addTransition(1, 2, 'a');
-        automaton.addTransition(2, 2, 'a');
-        automaton.addTransition(0, 3, 'e');
-        automaton.addTransition(3, 4, 'b');
-        automaton.addTransition(4, 4, 'b');
+        automaton.addTransition(0, 0, '0');
+        automaton.addTransition(0, 1, '0');
+        automaton.addTransition(1, 2, 'e');
+        automaton.addTransition(1, 3, 'e');
+        automaton.addTransition(2, 4, '1');
+        automaton.addTransition(3, 5, '0');
+        automaton.addTransition(5, 6, '5');
 
     }
 }
---
+++ Practice2/main.py
@@ -0,0 +1,6 @@
+from analyzer.analizer import Analyzer
+def main():
+    analyzer = Analyzer('a|b')
+    analyzer.analyze()
+
+main()
