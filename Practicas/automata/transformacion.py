63de6c06c559d481aa562d460af24812b527c706
Carlos Barrera <carlostonatihu@gmail.com>
2017-08-29 22:24:34 -0500

    Meh

---
+++ Practicas/automata/transformacion.py
@@ -0,0 +1,3 @@
+# Cosas bien chidas como la cerradura epsilon y los movimientos que los estados de este subconjunto pueden hacer
+# para pasar de un automata no deterministico a uno deterministico al puro estilo del libro de compiladores
+# mañana lo hago, solo es programar el algoritmo del libro y sha
--- Practicas/iniciar.py
+++ Practicas/iniciar.py
@@ -5,7 +5,7 @@ from expresiones.analizador import Analizador
 def correr_analizador():
     print("Ejecutando...")
     analizador = Analizador()
-    analizador.convertir_inorden('a*(b|cd)*')
+    analizador.convertir_inorden('(a|b)*abb')
     analizador.mostar_expresion_inorden()
     analizador.generar_automata()
     analizador.mostar_automata()
@@ -13,25 +13,26 @@ def correr_analizador():
 
 def correr_automata():
     automata = AFN()
-    automata.agregar_alfabeto({'1', 'x'})
+    automata.agregar_alfabeto({'a', 'b'})
     automata.agregar_estado(0)
     automata.agregar_estado(1)
     automata.agregar_estado(2)
     automata.agregar_estado(3)
 
     automata.agregar_inicial(0)
-    automata.agregar_finales({0, 3})
+    automata.agregar_finales({3})
 
-    automata.agregar_transicion(0, 1, '1')
-    automata.agregar_transicion(1, 0, '1')
-    automata.agregar_transicion(0, 2, 'e')
-    automata.agregar_transicion(2, 3, 'x')
+    automata.agregar_transicion(0, 1, 'a')
+    automata.agregar_transicion(1, 2, 'b')
+    automata.agregar_transicion(2, 3, 'b')
+    automata.agregar_transicion(0, 0, 'a')
+    automata.agregar_transicion(0, 0, 'b')
 
-    if automata.evaluar_cadena('111x111'):
+    if automata.evaluar_cadena('aaabbabbabb'):
         print("Cadena valida")
     else:
         print("Cadena no valida")
 
 
-# correr_analizador()
+#correr_analizador()
 correr_automata()
